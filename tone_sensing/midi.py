d4303ce7cbced8361d8123c84f06cdcb58580add
emrobbb <e.c.robbinson99@gmail.com>
2023-01-28 13:32:57 -0500

    testing mode added to tone sensing

--- tone_sensing/midi.py
+++ tone_sensing/midi.py
@@ -5,9 +5,9 @@ def print_devices():
     for n in range(pygame.midi.get_count()):
         print (n,pygame.midi.get_device_info(n))
 
-if __name__ == '__main__':
-    pygame.midi.init()
-    print_devices()
+#if __name__ == '__main__':
+    #pygame.midi.init()
+    #print_devices()
 
 notes = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b']
 keys = np.array([x+str(y) for y in range(0,9) for x in notes])
@@ -20,6 +20,13 @@ n=21
 for i in range(len(keys)):    
     midi_num.append(n)
     n=n+1
+
+c_major_scale_freqs=[]
+c_major_scale=['c4', 'd4', 'e4', 'f4', 'g4', 'a4', 'b4', 'c5']
+for i in range(len(keys)):
+    for j in range(8):
+        if c_major_scale[j]==keys[i]:
+            c_major_scale_freqs.append(midi_num[i])
 
 def number_to_note(number):
     index = midi_num.index(number)
@@ -36,8 +43,32 @@ def readInput(input_device):
             note_number = data[1]
             velocity = data[2]
             print (number_to_note(note_number), velocity)
+def similar(x,y):
+    si = 0
+    for a,b in zip(x, y):
+        if a == b:
+            si += 1
+    return (si/len(x)) * 100
 
+def testInput(input_device):
+    test=[]
+    while len(test)<16:
+        if input_device.poll():
+            event = input_device.read(1)[0]
+            
+            data = event[0]
+            #timestamp = event[1]
+            note_number = data[1]
+            #velocity = data[2]
+            test.append(number_to_note(note_number))
+    test=test[::2]
+    print(test)
+    res = len(set(c_major_scale) & set(test)) / float(len(set(c_major_scale) | set(test))) * 100
+    print(str(res))
+
+
+  
 if __name__ == '__main__':
     pygame.midi.init()
     my_input = pygame.midi.Input(0) #only in my case the id is 0
-    readInput(my_input)
+    testInput(my_input)
