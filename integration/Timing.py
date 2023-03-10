3857295c3d16a91fc93b30679332fb022cc84244
chandrac <chandrac@mcmaster.ca>
2023-03-10 17:07:32 -0500

    rolling back compatibility with previous functions

--- integration/Timing.py
+++ integration/Timing.py
@@ -1,68 +1,35 @@
-import array
-def create_song(song,bpm): 
+from playsound import playsound
+import time
+import constants
+import finger_placement
 
+def metronome(adjust,length):
+    loop_length = int(length/((1/4) * adjust))  ## integer value of loop length is how many quarter notes there are
     
-    bpm = 80
-    spb = 1/(bpm/60)
-
-    new_song=[ [0]*2 for i in range(len(song))]
-    for i in range(0,len(song)):
-        new_song[i][0]= song[i]
-        if song[i] == 1:#wholenote
-            new_song[i][1]= spb*4
-        elif song[i] == 2:#halfnote
-            new_song[i][1]= spb*2
-        elif song[i] == 4:#quarternote
-            new_song[i][1]= spb
-        elif song[i] == 8:#eighthnote
-            new_song[i][1]= spb/2
-        else: #sixteenthnote
-            new_song[i][1]= spb/4
-
-    #print (new_song)
-    return new_song
-    
-
- 
-
-    
-#new_song = create_song(song,beatsperbar,bpm) # put this in import song function
+    for i in range(0,int(loop_length)):
+        start_time = time.time()
+        #playsound('C:\\Users\\Garret\\Desktop\\Capstone\\mozartmaker\\integration\\ding.mp3',FALSE)        #garrets desktop
+        playsound('D:\\Users\\Documents\\Connor\\Connors Schoolwork\\ELEC ENG 4OI6\\mozartmaker\\integration\\ding.mp3', False)         #connors desktop  
+        time.sleep((0.25 * adjust) - (time.time() - start_time)) 
 
 
-    
-    #print (new_song)
+def timing_refactor(scale):
+    timed_song = constants.mhall        #needs to be changed to song that is imported with timing, not mhall
+    for i in range (0, len(scale)):
+        timed_song[i][1] = timed_song[i][1] * constants.sec_adjusted_bpm
+    return timed_song
 
+def timing_refactor_finger(scale, note_array):
+    timed_song_fingers = constants.mhall        #needs to be changed to song that is imported with timing, not mhall
+    fingers = finger_placement.fingers(scale, note_array)
+    for i in range (0, len(scale)):
+        timed_song_fingers[i][1] = timed_song_fingers[i][1] * constants.sec_adjusted_bpm
+        timed_song_fingers[i].append(fingers[i])
+    return timed_song_fingers
 
-
-
-
-# def create_bar(song,skip):
-#     check=0
-#     bar = []
-#     i=1
-#     while (check < 16):
-#         if i+skip >= len(song):
-#             break
-#         elif song[i+skip] == 1: #wholenote
-#             for j in range(0,16):
-#                 bar.append(1)
-#             check += 16
-#         elif song[i+skip] == 2: #halfnote
-#             for j in range(0,8):
-#                 bar.append(2)
-#             check += 8
-#         elif song[i+skip] == 4:#quarternote
-#             for j in range(0,4):
-#                 bar.append(4)
-#             check += 4
-#         elif song[i+skip] == 8:#eighthnote
-#             for j in range(0,2):
-#                 bar.append(8)
-#             check += 2
-#         else: #sixteenthnote           
-#             bar.append(16)
-#             check +=1
-#         i+=1
-#     print (bar, '\n \n')
-#     return bar
-            
+def finger_refactor(scale, note_array):
+    song_fingers = scale        #needs to be changed to song that is imported with timing, not mhall
+    fingers = finger_placement.fingers(scale, note_array)
+    for i in range (0, len(scale)):
+        song_fingers[i].append(fingers[i])
+    return song_fingers
--- integration/integration.py
+++ integration/integration.py
@@ -6,80 +6,45 @@ import projection
 import tone
 import time
 import midi
-import Timing
-import finger_placement
+import timing
 import threading
-from playsound import playsound
 
-def learning_mode(root, canvas, screen_width, screen_height, note_array, scale, keyboard):
+
+def learning_mode(root, canvas, screen_width, screen_height, note_array, scale, keyboard, finger_flag):
     i = 0
-    chord_check = 0
-    previous_note = 0
-    note_time = 0
     played_note = [0,0]
+    note_status = "green"
     #call function for displaying the first note to play
-    projection.project_key(root, canvas, screen_width, screen_height, note_array, i)
+    if (finger_flag == "y"):
+        song_fingerings = timing.finger_refactor(scale, note_array)
+        print("note array :", note_array)
+        finger_param = str(song_fingerings[i][2])
+        print(song_fingerings)
+    else:
+        finger_param = ""
+
+    projection.project_key(root, canvas, screen_width, screen_height, note_array, i, note_status, finger_param)
     
     while i < len(scale):
         played_note = midi.note_stream(keyboard)
         if (played_note):
-            if played_note[0] == scale[i]:
-                note_time = played_note[2]
-                print("played note: ", played_note)
-                # if (note_time - previous_note < 0.05):
-                #     chord_check = TRUE
-                #     print("CHORD DETECTED")
-                print("Played note is: ", played_note)
-                print("Note is correct!\n")
+            if played_note[0] == scale[i][0]:
+                # print("played note: ", played_note)
+                # print("Played note is: ", played_note)
+                # print("Note is correct!\n")
                 projection.project_white(root, canvas, screen_width, screen_height, note_array, i)
                 i += 1
                 if (i == len(scale)):
                     break
-                projection.project_key(root, canvas, screen_width, screen_height, note_array, i)
-            #previous_note = note_time
-
-def timing_refactor(scale):
-    timed_song = constants.mhall        #needs to be changed to song that is imported with timing, not mhall
-    for i in range (0, len(scale)):
-        timed_song[i][1] = timed_song[i][1] * constants.sec_adjusted_bpm
-    return timed_song
-
-def timing_refactor_finger(scale, note_array):
-    timed_song_fingers = constants.mhall        #needs to be changed to song that is imported with timing, not mhall
-    fingers = finger_placement.fingers(scale, note_array)
-    for i in range (0, len(scale)):
-        timed_song_fingers[i][1] = timed_song_fingers[i][1] * constants.sec_adjusted_bpm
-        timed_song_fingers[i].append(fingers[i])
-    return timed_song_fingers
-
-
-def metronome(adjust,length):
-    loop_length = int(length/((1/4) * adjust))
-    
-    for i in range(0,int(loop_length)):
-        start_time = time.time()
-        playsound('C:\\Users\\Garret\\Desktop\\Capstone\\mozartmaker\\integration\\ding.mp3',FALSE)
-        end_time= time.time()
-        time.sleep((0.25 * adjust) - (time.time() - start_time))
-        
+                projection.project_key(root, canvas, screen_width, screen_height, note_array, i, note_status, finger_param)
 
 def learning_mode_timing(root, canvas, screen_width, screen_height, note_array, scale, keyboard):
     i = 0
-    chord_check = 0
-    previous_note = NONE
-    note_time = 0
     # song_bpm_adjust = timing_refactor(scale)
-    song_bpm_adjust = timing_refactor_finger(scale, note_array)
+    song_bpm_adjust = timing.timing_refactor_finger(scale, note_array)
     make_times=[]
     note_status = "green"
-    song_length = 0
-    for i in range (0,len(scale)):
-        song_length+=scale[i][1]
-    song_length+=8*(.25*constants.sec_adjusted_bpm)
-        
-    x = threading.Thread(target=metronome, args=(constants.sec_adjusted_bpm,song_length))
-    x.start()
-    count_in()   
+           
     for i in range (0,len(scale)):
         
         #call function for displaying the first note to play
@@ -92,91 +57,27 @@ def learning_mode_timing(root, canvas, screen_width, screen_height, note_array, 
                 if played_note[1] == 100:
                     if played_note[0] == scale[i][0]:
                         make_times.append(played_note[2])
-                        #print(make_times)
 
                 elif (played_note[0] == scale[i][0] or played_note[0] == scale[i-1][0]):
                     break_time = played_note[2]
-                    #print("break_time",break_time)
                     try:
                         make_time = make_times[0]
                     except IndexError:
                         note_status = "orange"
-                        make_time = break_time
-                    #print("make time",make_time)                        
+                        make_time = break_time           
                     time_on_note = abs(break_time - make_time)
-                    #print("time on note (ms): ", time_on_note)
                     time_on_note /= 1000
                     print("played time", time_on_note)
                     if (time_on_note > (constants.ERROR * song_bpm_adjust[i][1])) and (time_on_note < ((2 - constants.ERROR) * song_bpm_adjust[i-1][1])):
                         note_status = "green"
                     else:
                         note_status = "orange"
-                    #print("note status: ", note_status)
                     try:    
                         make_times.pop(0)   
                     except Exception:
                         pass
-
-                    #print("Played note is: ", played_note)
-                    #print("Note is correct!\n")
-                    #i += 1
-                    #previous_note=played_note
         white_time = projection.project_white(root, canvas, screen_width, screen_height, note_array, i)    
         time.sleep(constants.WHITE_TIME - white_time)
-
-def testing_mode_timing(scale, keyboard):
-    i = 0
-    chord_check = 0
-    song_bpm_adjust = timing_refactor(scale)
-    make_times=[]
-    correct_notes=0
-    correct_times=0
-
-    for i in range (0,len(scale)):
-        
-        #call function for displaying the first note to play
-        note_start = time.time()
-        #print ('dog')
-        while (note_start + song_bpm_adjust[i][1]) > time.time():
-            played_note = midi.note_stream(keyboard)
-            if (played_note):
-                print(played_note)
-                if played_note[1] == 100:
-                    print("1")
-                    if played_note[0] == scale[i][0]:
-                        correct_notes += 1
-                        print("2")
-                        make_times.append(played_note[2])
-                        #print(make_times)
-
-                elif (played_note[0] == scale[i][0] or played_note[0] == scale[i-1][0]):
-                    break_time = played_note[2]
-                    print("3")
-                    #print("break_time",break_time)
-                    try:
-                        make_time = make_times[0]
-                        print("4")
-                    except IndexError:
-                        make_time=break_time
-                        print("5")
-                    #print("make time",make_time)
-                    time_on_note = abs(break_time - make_time)
-                    #print("time on note (ms): ", time_on_note)
-                    time_on_note /= 1000
-                    print("played time", time_on_note)
-                    if (time_on_note > (constants.ERROR * song_bpm_adjust[i][1])) and (time_on_note < ((2 - constants.ERROR) * song_bpm_adjust[i-1][1])):
-                        correct_times+=1
-
-                    #print("note status: ", note_status)
-                    try:
-                        make_times.pop(0) 
-                    except Exception:
-                        pass  
-    result_note = float(correct_notes / len(scale)) * 100
-    result_time=float(correct_times / len(scale)) * 100
-    print("Your correct note score is: ", round(result_note, 1), "%")
-    print("Your time note score is: ", round(result_time, 1), "%")
-    print("Your overall score is: ", round((result_note+result_time)/2))
 
 def testing_mode(scale, keyboard):
     i = 0
@@ -190,13 +91,50 @@ def testing_mode(scale, keyboard):
                 i += 1
         if (i == len(scale)):
             break    
-                
-    # for n in range(len(scale)):
-    #     if (test_notes[n] == scale[n]):
-    #         correct_notes += 1
 
     result = float(correct_notes / len(scale)) * 100
     print("Your test score is: ", round(result, 1), "%")
+
+def testing_mode_timing(scale, keyboard):
+    i = 0
+    song_bpm_adjust = timing.timing_refactor(scale)
+    make_times=[]
+    correct_notes=0
+    correct_times=0
+
+    for i in range (0,len(scale)):
+        
+        #call function for displaying the first note to play
+        note_start = time.time()
+        while (note_start + song_bpm_adjust[i][1]) > time.time():
+            played_note = midi.note_stream(keyboard)
+            if (played_note):
+                if played_note[1] == 100:
+                    if played_note[0] == scale[i][0]:
+                        correct_notes += 1
+                        make_times.append(played_note[2])
+
+                elif (played_note[0] == scale[i][0] or played_note[0] == scale[i-1][0]):
+                    break_time = played_note[2]
+                    try:
+                        make_time = make_times[0]
+                    except IndexError:
+                        make_time=break_time
+                    time_on_note = abs(break_time - make_time)
+                    time_on_note /= 1000
+                    if (time_on_note > (constants.ERROR * song_bpm_adjust[i][1])) and (time_on_note < ((2 - constants.ERROR) * song_bpm_adjust[i-1][1])):
+                        correct_times+=1
+
+                    try:
+                        make_times.pop(0) 
+                    except Exception:
+                        pass  
+    result_note = float(correct_notes / len(scale)) * 100
+    result_time=float(correct_times / len(scale)) * 100
+    print("Your correct note score is: ", round(result_note, 1), "%")
+    print("Your time note score is: ", round(result_time, 1), "%")
+    print("Your overall score is: ", round((result_note+result_time)/2))
+
 
 def count_in():
     first_note = 0
@@ -247,29 +185,37 @@ if __name__ == "__main__":
     #if not((play_mode == "test") or (play_mode == "learn")):
     #    raise Exception("Not a valid mode. Please try again\n")
 
+    finger_flag = input("Would you like to play with finger markings? (y/n)\n")
+
     projection_index = []
     #print("length of scale: ", len(scale))
     note_array = np.zeros((len(scale), constants.total_keys))
     for i in range(len(scale)):
-        if (play_mode == 'learntime') or (play_mode == 'testtime'):
-            projection_index.append(projection_luts.note_lut(scale[i][0]))
-        else:
-            projection_index.append(projection_luts.note_lut(scale[i]))
-        
-        
-
+        # if (play_mode == 'learntime') or (play_mode == 'testtime'):
+        projection_index.append(projection_luts.note_lut(scale[i][0]))
+        # else:
+        #     projection_index.append(projection_luts.note_lut(scale[i]))
+    
     for n in range(len(scale)):
         note_array[n][projection_index[n]] = 1
 
-    # values = timing_refactor_finger(scale, note_array)
-    # print("values: ", values)
-    # count_in()    
+    song_length = 0
+    for i in range (0,len(scale)):
+        song_length += scale[i][1]
+    song_length += 8 * (0.25 * constants.sec_adjusted_bpm)
+    print("song length", song_length)
+
     if (play_mode == "learn"):
-        learning_mode(root, canvas, screen_width, screen_height, note_array, scale, keyboard)
+        learning_mode(root, canvas, screen_width, screen_height, note_array, scale, keyboard, finger_flag)
     elif (play_mode == "test"):
         testing_mode(scale, keyboard)
     elif (play_mode == "testtime"):
+        x = threading.Thread(target = timing.metronome, args=(constants.sec_adjusted_bpm, song_length))
+        x.start()
+        count_in() 
         testing_mode_timing(scale, keyboard)
     elif (play_mode == "learntime"):
+        x = threading.Thread(target = timing.metronome, args=(constants.sec_adjusted_bpm, song_length))
+        x.start()
+        count_in() 
         learning_mode_timing(root, canvas, screen_width, screen_height, note_array, scale, keyboard)
-    
